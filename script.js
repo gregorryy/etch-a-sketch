35149676a9062afcfdd206f765e5370bddf5d606
greg <g.coraboeuf84@gmail.com>
2024-11-09 17:16:37 +0100

    Color button, restart button, change grid size button, multi selection mode

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let color = "white";
+
+const redBtn = document.querySelector("#red");
+
+redBtn.addEventListener("click", () => {
+  color = "red";
+});
+
+const greenBtn = document.querySelector("#green");
+
+greenBtn.addEventListener("click", () => {
+  color = "green";
+});
+
+const blueBtn = document.querySelector("#blue");
+
+blueBtn.addEventListener("click", () => {
+  color = "blue";
+});
+
+const whiteBtn = document.querySelector("#white");
+
+whiteBtn.addEventListener("click", () => {
+  color = "white";
+});
+
+const randomBtn = document.querySelector("#random");
+
+randomBtn.addEventListener("click", () => {
+  color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+});
+
 function grid(size) {
   const container = document.querySelector("#container");
   container.innerHTML = "";
@@ -6,24 +38,54 @@ function grid(size) {
 
   const gridSize = size * size;
   for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement("div");
+    const div = document.createElement("button");
     div.classList.add("sketch-div");
     container.appendChild(div);
   }
-  const hoverDiv = document.querySelectorAll(".sketch-div");
 
-  hoverDiv.forEach((item) => {
-    item.addEventListener("mouseenter", function () {
-      item.style.backgroundColor =
-        "#" + ((Math.random() * 0xffffff) << 0).toString(16);
-      item.style.border = "1px solid white";
+  const buttons = document.querySelectorAll(".sketch-div");
+
+  let isMouseDown = false;
+
+  // Lorsqu'on commence à appuyer sur le bouton
+  document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+
+  // Lorsqu'on relâche le clic de la souris
+  document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+
+  // Lorsqu'un bouton est cliqué ou survolé en maintenant le clic
+  buttons.forEach((button) => {
+    button.addEventListener("mousedown", (event) => {
+      if (isMouseDown) {
+        event.target.style.backgroundColor = color;
+        event.target.style.border = "1px solid black";
+      }
+    });
+
+    // Gérer le cas où l'utilisateur maintient le clic sur plusieurs boutons
+    button.addEventListener("mouseover", (event) => {
+      if (isMouseDown) {
+        event.target.style.backgroundColor = color;
+        event.target.style.border = "1px solid black";
+      }
+    });
+    button.addEventListener("click", (event) => {
+      event.target.style.backgroundColor = color;
+      event.target.style.border = "1px solid black";
     });
   });
+
+  return size;
 }
 
 grid(16);
-const restartBtn = document.querySelector(".restartBtn");
-restartBtn.addEventListener("click", () => {
+
+const sizeBtn = document.querySelector("#changeSize");
+sizeBtn.addEventListener("click", () => {
   let userGridSize = prompt("Enter grid size (max 100):", "16");
   if (userGridSize > 100) {
     alert("Grid size too large. Please enter a number between 1 and 100.");
@@ -33,5 +95,11 @@ restartBtn.addEventListener("click", () => {
     userGridSize = "16"; // Valeur par défaut
   }
   grid(userGridSize);
-  return userGridSize;
+});
+
+const restartBtn = document.querySelector(".restartBtn");
+restartBtn.addEventListener("click", () => {
+  let number = document.querySelectorAll(".sketch-div").length;
+  let userGridSize = Math.sqrt(number);
+  grid(userGridSize);
 });
